10255d6559c7f17f9a7a288610d38d76656dcf31
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-15 18:09:30 +0530

    Update fix.

--- frontend/README.md
+++ frontend/README.md
@@ -1,5 +1,11 @@
 # Calendar Imroved
 
-npm - https://www.npmjs.com/package/react-date-range
-github - https://github.com/hypeserver/react-date-range
-demo - https://hypeserver.github.io/react-date-range/
+- `react-date-range`:
+	- npm - https://www.npmjs.com/package/react-date-range
+	- github - https://github.com/hypeserver/react-date-range
+	- demo - https://hypeserver.github.io/react-date-range/
+
+TODO:
+- `react-datepicker`
+	- npm - https://www.npmjs.com/package/react-datepicker
+	- Make this project using: [Click here](https://reactdatepicker.com/#example-date-range-for-one-datepicker)
--- frontend/src/components/MyCalendar.tsx
+++ frontend/src/components/MyCalendar.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
-import { DateRangePicker, RangeKeyDict } from 'react-date-range';
+import { DateRangePicker, RangeKeyDict, createStaticRanges, defaultStaticRanges } from 'react-date-range';
 import { getEventsByRange } from '../api/events';
 import { EventType } from '../types';
 import { formatDate } from '../date-utils';
+import { endOfISOWeek, isSameDay, startOfISOWeek, subDays } from 'date-fns';
 
 type SelectionRange = {
 	startDate?: Date
@@ -36,8 +37,9 @@ export default function MyCalendar() {
 	}, [])
 
 	const handleSelect = async (rangesByKey: RangeKeyDict) => {
-		const isSameDay = rangesByKey.selection.startDate === rangesByKey.selection.endDate;
 		let selected: SelectionRange;
+
+		const isSameDay = rangesByKey.selection.startDate === rangesByKey.selection.endDate;
 		if (isSameDay) {
 			const endDate = rangesByKey.selection.startDate!;
 			const startOfDay = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), 0, 0, 0, 0);
@@ -51,7 +53,6 @@ export default function MyCalendar() {
 			const endDate = rangesByKey.selection.endDate!;
 			const startOfDay = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), 0, 0, 0, 0);
 			const endOfDay = new Date(startOfDay.getTime() + (24 * 60 * 60 * 1000) - 1);
-
 			selected = {
 				startDate: rangesByKey.selection.startDate,
 				endDate: endOfDay,
@@ -63,29 +64,87 @@ export default function MyCalendar() {
 		const res = await getEventsByRange(selected.startDate!.toJSON(), selected.endDate!.toJSON())
 		setEvents(res.data)
 
-		// console.log('rangesByKey.selection.key?', rangesByKey.selection.key);
-		// OUTPUT: rangesByKey
-		// {
-		//   selection: {
-		//     startDate: [native Date Object],
-		//     endDate: [native Date Object],
-		//   }
-		// }
+		// TODO: Make duplidate for specific dates for above calendar days
+		// TODO: For this I need to multiple `event` items and then call `setEvents` with those duplicate events.
+		// const recurrence = res.data.map((item: any) => item.recurrence);
+		// const datesNumbers = recurrence.map((rec: string[]) => {
+		// 	return rec.map((r) => {
+		// 		const splitted = r?.split(';');
+		// 		const found = splitted.find((r: any) => r?.startsWith('BYMONTHDAY'));
+		// 		const datesNumber = found?.split('=')[1];
+		// 		return datesNumber;
+		// 	})
+		// });
+		// console.log('datesNumber?', datesNumbers); // OUTPUT: [ "5,6,7,8,9,10" ]
 	}
+
+	// DEFAULT STATIC RANGES:
+	// ======================
+	// Today
+	// Yesterday
+	// This Week
+	// Last Week
+	// This Month
+	// Last Month
+	const staticRanges = createStaticRanges([
+		defaultStaticRanges.find((staticRange) => staticRange.label === 'Today') as any,
+		defaultStaticRanges.find((staticRange) => staticRange.label === 'Yesterday') as any,
+		{
+			label: 'Last week',
+			range: () => ({
+				startDate: startOfISOWeek(subDays(new Date(), 7)),
+				endDate: endOfISOWeek(subDays(new Date(), 7)),
+				key: 'selection'
+			}),
+			isSelected: (range) => {
+				if (range.startDate && range.endDate) {
+					return (
+						isSameDay(range.startDate, startOfISOWeek(subDays(new Date(), 7))) &&
+						isSameDay(range.endDate, endOfISOWeek(subDays(new Date(), 7)))
+					)
+				}
+				return false;
+			}
+		},
+		{
+			label: 'This week',
+			range: () => ({
+				startDate: startOfISOWeek(new Date()),
+				endDate: endOfISOWeek(new Date()),
+				key: 'selection'
+			}),
+			isSelected: (range) => {
+				if (range.startDate && range.endDate) {
+					return (
+						isSameDay(range?.startDate, startOfISOWeek(new Date())) &&
+						isSameDay(range?.endDate, endOfISOWeek(new Date()))
+					)
+				}
+				return false;
+			}
+		},
+		defaultStaticRanges.find((staticRange) => staticRange.label === 'This Month') as any,
+		defaultStaticRanges.find((staticRange) => staticRange.label === 'Last Month') as any,
+		// Ad other ranges as per need
+	]);
+
 	return (
 		<div>
 			<DateRangePicker
 				className='any_class_i_need'
 				ranges={[selectionRange]}
 				onChange={handleSelect}
+				staticRanges={staticRanges}
 				weekStartsOn={1}
+				// showPreview={false} // To disable hovering preview
+				inputRanges={[]} // Disable `Days Upto` feature
 			/>
 
 			<h1>Events</h1>
 			{events?.map((event) =>
 				<div
 					id={event.id}>
-					{formatDate(event.start.dateTime)} | {event.summary}
+					{formatDate(event.start?.dateTime)} | {event.summary}
 				</div>)}
 		</div>
 	);
